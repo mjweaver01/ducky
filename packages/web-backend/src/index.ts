0d5df5c3cc614f52f074b9cf4abcb4d998700819
mjweaver01 <8439630+mjweaver01@users.noreply.github.com>
2026-02-28 22:33:14 -0500

    Rate limit

--- packages/web-backend/src/index.ts
+++ packages/web-backend/src/index.ts
@@ -57,7 +57,7 @@ app.use(express.urlencoded({ extended: true }));
 // Rate limiting
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per window
+  max: 1000, // Limit each IP to 1000 requests per window
   message: 'Too many requests from this IP, please try again later.',
 });
 app.use('/api/', limiter);
